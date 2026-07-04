798696f2f61f4604d50d8b837b7bfe7cc837bf81
abhirupsg99 <abhirupsengupta2005@gmail.com>
2026-07-05 02:04:46 +0530

    Time: 34 ms (74.26%), Space: 53.5 MB (83.26%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,32 +1,46 @@
 class Solution {
 public:
-    bool isPalindrome(string &s,int l,int r){
-        while(l<r){
-            if(s[l] != s[r])
-            return false;
-            l++;
-            r--;
+    void backtrack(string &s, int idx, vector<string> &curr,
+                   vector<vector<string>> &result,
+                   vector<vector<bool>> &pal) {
+
+        if (idx == s.size()) {
+            result.push_back(curr);
+            return;
         }
-        return true;
-    }
-    void backtrack(string &s, int idx, vector<string> &curr,  vector<vector<string>> &result){
-        if(idx==s.length())
-        {result.push_back(curr);
-        return;
-        }
-        for(int i=idx;i<s.length();i++){
-            if(isPalindrome(s,idx,i)){
-                curr.push_back(s.substr(idx,i-idx+1));
-                backtrack(s,i+1,curr,result);
+
+        for (int i = idx; i < s.size(); i++) {
+            if (pal[idx][i]) {
+                curr.push_back(s.substr(idx, i - idx + 1));
+                backtrack(s, i + 1, curr, result, pal);
                 curr.pop_back();
             }
         }
-
     }
+
     vector<vector<string>> partition(string s) {
-           vector<vector<string>> result;
-           vector<string> curr;
-           backtrack(s,0,curr,result);
-           return result;
+
+        int n = s.size();
+        vector<vector<bool>> pal(n, vector<bool>(n, false));
+
+        for (int len = 1; len <= n; len++) {
+            for (int i = 0; i + len - 1 < n; i++) {
+                int j = i + len - 1;
+
+                if (len == 1)
+                    pal[i][j] = true;
+                else if (len == 2)
+                    pal[i][j] = (s[i] == s[j]);
+                else
+                    pal[i][j] = (s[i] == s[j] && pal[i + 1][j - 1]);
+            }
+        }
+
+        vector<vector<string>> result;
+        vector<string> curr;
+
+        backtrack(s, 0, curr, result, pal);
+
+        return result;
     }
 };
