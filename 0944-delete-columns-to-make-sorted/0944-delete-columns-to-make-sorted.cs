7123b42b47b3974644d1202ccc0421b76d373ed3
abhirupsg99 <abhirupsengupta2005@gmail.com>
2026-07-04 15:11:19 +0530

    Time: 4 ms (49.18%), Space: 48 MB (16.39%) - LeetHub

---
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int MinDeletionSize(string[] strs) {
+        int n=strs.Length;
+        int k=strs[0].Length;
+        int cnt=0;
+        for(int i=0;i<k;i++){
+            for(int j=1;j<n;j++){
+                if(strs[j][i]<strs[j-1][i])
+                {cnt++;
+                break;
+                }
+            }
+        }
+        return cnt;
+    }
+}
